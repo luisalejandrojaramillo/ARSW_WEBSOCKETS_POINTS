86d50a6fecf1824b3dda6bda0753f053f3870613
luisalejandrojaramillo <luis.jaramillo-r@mail.escuelaing.edu.co>
2020-03-19 10:11:07 -0500

    Parte II

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -8,12 +8,13 @@ var app = (function () {
     }
 
     var stompClient = null;
+    var id;
 
     var addPointToCanvas = function (point) {
         var canvas = document.getElementById("canvas");
         var ctx = canvas.getContext("2d");
         ctx.beginPath();
-        ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
+        ctx.arc(point.x, point.y, 1, 0, 2 * Math.PI);
         ctx.stroke();
     };
 
@@ -32,7 +33,6 @@ var app = (function () {
         console.info('Connecting to WS...');
         var socket = new SockJS('/stompendpoint');
         stompClient = Stomp.over(socket);
-
         //subscribe to /topic/TOPICXX when connections succeed
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
@@ -49,16 +49,18 @@ var app = (function () {
     return {
 
         init: function () {
+            id = $("#nDib").val();
             var can = document.getElementById("canvas");
             can.addEventListener("pointerdown",function (evento) {
                 var position = getMousePosition(evento);
                 app.publishPoint(position.x,position.y);
             })
             //websocket connection
-            connectAndSubscribe(alert);
+            connectAndSubscribe(addPointToCanvas);
         },
 
         publishPoint: function(px,py){
+            console.info(id);
             var pt=new Point(px,py);
             console.info("publishing point at "+pt);
             addPointToCanvas(pt);
